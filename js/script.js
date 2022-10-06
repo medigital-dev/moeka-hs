cace2fd4f60d3a042e5ca1bdfa5c7b8fad624713
@mesaidlg <medigital.net@gmail.com>
2022-10-06 11:31:47 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -4,5 +4,15 @@ $(document).ready(function(){
     const year1 = date.getFullYear();
     var tp = (year1+1)+"/"+(year1+2);
     $('#tp').text(tp);
-    console.log(tp);
+
+    $('#connect').click(function(e){
+        e.preventDefault();
+        const username = $('#username');
+        if(username.val()==''){
+            username.addClass('is-invalid');
+        } else {
+            $(this).html('<div class="spinner-border spinner-border-sm" role="status"><span class="visually-hidden">Loading...</span></div>');
+            $('#formLogin').trigger('submit');
+        }
+    })
 });
